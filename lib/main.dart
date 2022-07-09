e128deeef164f25fa7ecdc163809cb3baab740f1
Abdul Ghofur <abdulghofur.me@gmail.com>
2022-07-09 16:11:19 +0700

    initiation

---
+++ lib/main.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main(List<String> args) {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    print('Rendered');
+    final countController = Get.put(Controller());
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('GETX State Management'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Obx(
+              () {
+                print('Only rendered observed data');
+                return Text(
+                  '${countController.count.value}',
+                  style: TextStyle(fontSize: 40),
+                );
+              },
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    countController.decrement();
+                  },
+                  child: Text('-'),
+                ),
+                SizedBox(
+                  width: 20,
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    countController.increment();
+                  },
+                  child: Text('+'),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Controller extends GetxController {
+  var count = 0.obs; // state yang akan diobservasi / dipantau perubahannya
+
+  increment() => count++;
+  decrement() => count--;
+}

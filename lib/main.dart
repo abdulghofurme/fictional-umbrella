668e70e8d7bd3c42da2ee8e1a984b5fd63ddeef6
Abdul Ghofur <abdulghofur.me@gmail.com>
2022-07-09 18:07:10 +0700

    GetX: Dialog & Bottom Sheet

--- lib/main.dart
+++ lib/main.dart
@@ -28,61 +28,98 @@ class HomePage extends StatelessWidget {
         title: Text('GETX State Management'),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Obx(
-              () {
-                print('Only rendered observed data');
-                return Text(
-                  '${countController.count.value}',
-                  style: TextStyle(fontSize: 40),
-                );
-              },
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                ElevatedButton(
-                  onPressed: () {
-                    countController.decrement();
-                  },
-                  child: Text('-'),
-                ),
-                SizedBox(
-                  width: 20,
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    countController.increment();
-                  },
-                  child: Text('+'),
-                ),
-              ],
-            ),
-            ElevatedButton(
-              onPressed: () {
-                // ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                //   content: Text('Hello'),
-                //   action: SnackBarAction(
-                //     label: "Label",
-                //     onPressed: () {},
-                //   ),
-                // ));
-                Get.snackbar(
-                  'Hai',
-                  'Ini adalah pesannya',
-                  snackPosition: SnackPosition.BOTTOM,
-                  backgroundColor: Colors.black,
-                  colorText: Colors.white,
-                );
-              },
-              child: Text('Snackbar'),
-            )
-          ],
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Obx(
+                () {
+                  print('Only rendered observed data');
+                  return Text(
+                    '${countController.count.value}',
+                    style: TextStyle(fontSize: 40),
+                  );
+                },
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      countController.decrement();
+                    },
+                    child: Text('-'),
+                  ),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      countController.increment();
+                    },
+                    child: Text('+'),
+                  ),
+                ],
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  // ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                  //   content: Text('Hello'),
+                  //   action: SnackBarAction(
+                  //     label: "Label",
+                  //     onPressed: () {},
+                  //   ),
+                  // ));
+                  Get.snackbar(
+                    'Hai',
+                    'Ini adalah pesannya',
+                    snackPosition: SnackPosition.BOTTOM,
+                    backgroundColor: Colors.black,
+                    colorText: Colors.white,
+                  );
+                },
+                child: Text('Snackbar'),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  // showDialog(
+                  //   context: context,
+                  //   builder: (context) => AlertDialog(
+                  //     title: Text('Judul'),
+                  //     content: Text('Description'),
+                  //   ),
+                  // );
+
+                  // Get.defaultDialog(
+                  //   title: 'Judul Getx',
+                  //   middleText: 'Description',
+
+                  // );
+
+                  Get.dialog(AlertDialog(
+                    title: Text('Judul'),
+                    content: Text('Description'),
+                  ));
+                },
+                child: Text('Dialog'),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  // showModalBottomSheet(
+                  //     context: context, builder: (context) => Container());
+
+                  Get.bottomSheet(Container(
+                    height: 300,
+                    color: Colors.white,
+                  ));
+                },
+                child: Text('Bottom Sheet'),
+              )
+            ],
+          ),
         ),
       ),
     );
